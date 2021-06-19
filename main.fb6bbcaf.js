508b36d59e309ccc990eb6fb71c5327a05d8feee
Andrew Brampton <bramp@google.com>
2021-06-19 12:18:35 -0700

    Updates

--- main.fb6bbcaf.js
+++ main.fb6bbcaf.js
@@ -10940,7 +10940,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64365" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50552" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
